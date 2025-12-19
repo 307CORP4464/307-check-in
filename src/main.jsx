4782e194ba85629d5176a426972b8d9568f82073
307CORP4464 <307corp@gmail.com>
2025-12-19 15:38:50 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import DriverCheckIn from "./DriverCheckIn";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<DriverCheckIn />} />
+      <Route path="/csr" element={<App />} />
+      <Route path="/admin" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 );
