87d8bd09aa0a89eaf4cb82b3c8ecab8310ee383e
307CORP4464 <307corp@gmail.com>
2025-12-22 12:26:04 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App";
 import DriverCheckIn from "./DriverCheckIn";
+import CSRDashboard from "./CSRDashboard";
+import AdminDashboard from "./AdminDashboard";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
+      {/* Public */}
       <Route path="/check-in" element={<DriverCheckIn />} />
-      <Route path="/*" element={<App />} />
+
+      {/* Auth + Layout */}
+      <Route path="/" element={<App />}>
+        <Route path="csr-dashboard" element={<CSRDashboard />} />
+        <Route path="admin" element={<AdminDashboard />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 );
