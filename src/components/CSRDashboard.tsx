a02fa48be456a7b641ac2caf7ad0eb5a35763cac
307CORP4464 <307corp@gmail.com>
2025-12-27 15:54:15 -0500

    Update CSRDashboard.tsx

--- src/components/CSRDashboard.tsx
+++ src/components/CSRDashboard.tsx
@@ -300,8 +300,11 @@ return (
                           </span>
                         </td>
                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                          {formatTimeInIndianapolis(ci.check_in_time)}
-                        </td>
+  <div>DB: {ci.check_in_time}</div>
+  <div>FN: {formatTimeInIndianapolis(ci.check_in_time)}</div>
+  <div>MANUAL: {new Date(ci.check_in_time).getUTCHours() - 5}:{String(new Date(ci.check_in_time).getUTCMinutes()).padStart(2, '0')}</div>
+</td>
+
                         <td className="px-6 py-4 whitespace-nowrap text-sm font-bold text-gray-900">
                           {ci.pickup_number || '-'}
                         </td>
